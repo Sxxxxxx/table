f074a33368bee7d7fa3132de9a96c6bc2425b7b6
sx <sx@sxdeMacBook-Pro.local>
2017-10-24 10:39:19 +0800

    删除无关文件

--- app/src/main/java/com/example/sx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sx/myapplication/MainActivity.java
@@ -26,14 +26,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        View decorView = getWindow().getDecorView();
-//        int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-//        decorView.setSystemUiVisibility(option);
         setContentView(R.layout.activity_main);
     }
-
-
 }
